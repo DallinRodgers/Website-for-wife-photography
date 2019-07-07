1f7f5f32400dce323c28146f1573f1a83696c5fd
Dallin Rodgers <rodgers.dallin@gmail.com>
2019-07-07 17:03:47 -0600

    Update index.html
    added gallery images and content

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const navSlide = () => {
-  const body = document.querySelector("body");
-  const showcase = document.querySelector(".showcase__content");
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-links li");
@@ -8,8 +6,6 @@ const navSlide = () => {
   // Toggle Nav
   burger.addEventListener("click", function() {
     nav.classList.toggle("nav-active");
-    body.classList.toggle("dark-overlay");
-    showcase.classList.toggle("dark-overlay");
 
     //   Animage Links
     // index represents the index of each li in the array
